8251aae76554ec9c72c8610c49baf46919b5aa89
helgelol <helge@kubes.no>
2022-05-30 12:25:33 +0200

    Added new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { DetailsComponent } from './components/details/details.component';
+import { FrontpageComponent } from './components/frontpage/frontpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FrontpageComponent,
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
