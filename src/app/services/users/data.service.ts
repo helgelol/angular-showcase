ef6ea1566ecb0ceb19fcb3d6cb62599e642e499a
helgelol <helge@kubes.no>
2022-06-07 14:48:32 +0200

    Removed specific user func

--- src/app/services/users/data.service.ts
+++ src/app/services/users/data.service.ts
@@ -10,8 +10,4 @@ export class DataService {
   getUsers() {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
-
-  getUser(userId: string) {
-    return this.http.get('https://jsonplaceholder.typicode.com/users' + userId);
-  }
 }
