5329acbbef38b314937f3e706270db0a41f4bde8
helgelol <helge@kubes.no>
2022-06-07 12:55:33 +0200

    Added emissions page

---
+++ src/app/pages/emissions/emissions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emissions',
+  templateUrl: './emissions.component.html',
+  styleUrls: ['./emissions.component.css']
+})
+export class EmissionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
