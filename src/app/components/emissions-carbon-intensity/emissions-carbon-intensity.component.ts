7a63fb40745e53b5b89b7e53aa4a7634a49d55bd
helgelol <helge@kubes.no>
2022-07-06 09:39:47 +0200

    Cleaned up unnecessary code

--- src/app/components/emissions-carbon-intensity/emissions-carbon-intensity.component.ts
+++ src/app/components/emissions-carbon-intensity/emissions-carbon-intensity.component.ts
@@ -12,13 +12,10 @@ import {
 })
 export class EmissionsCarbonIntensityComponent {
   public options: any;
-  selectedDeviceObj!: { name: any };
   carbonDataCountry?: String;
   carbonDataIndexNumber?: Number;
   carbonDataIndex?: String;
-  carbonDataMix?: any;
-  chartValues?: any = [];
-  chartNames?: any = [];
+  carbonDataMix?: any = [];
   pieChartData?: any = [];
 
   constructor(private data: CarbonService) {}
@@ -50,7 +47,6 @@ export class EmissionsCarbonIntensityComponent {
         this.carbonDataMix.forEach((element: any) => {
           this.pieChartData.push({ label: element.fuel, value: element.perc });
         });
-        console.log('Data fetched');
 
         this.options = {
           data: this.pieChartData,
@@ -62,22 +58,7 @@ export class EmissionsCarbonIntensityComponent {
             },
           ],
         };
-        console.log('Chart should be drawn now');
       },
     });
   }
-
-  //   this.showChart = false;
-  //   this.chartValues = [];
-  //   this.chartNames = [];
-
-  //   this.carbonDataMix = this.receivedCarbonData.data[0].data[0].generationmix;
-
-  //   this.carbonDataMix.forEach((element: any) => {
-  //     this.chartValues.push(element.perc);
-  //     this.chartNames.push(element.fuel);
-  //   });
-
-  //   // Setting this to true makes the canvas draw
-  //   this.showChart = true;
 }
--- src/app/services/emissions/carbon.service.ts
+++ src/app/services/emissions/carbon.service.ts
@@ -8,17 +8,8 @@ import { Observable } from 'rxjs';
 export class CarbonService {
   receivedCarbonData!: void;
   constructor(private http: HttpClient) {}
-  // Declarations
-  selectedCountry: string = '';
+
   preUrlEndpoint: string = 'https://api.carbonintensity.org.uk/regional/';
-  endpoint: string = '';
-  carbonData: any = [];
-  carbonDataIndex: string = '';
-  carbonDataIndexNumber: string = '';
-  carbonDataMix: any = [];
-  showChart: Boolean = false;
-  chartValues: any = [];
-  chartNames: any = [];
 
   getCarbonData(country: string): Observable<CarbonDataModel> {
     return this.http.get<CarbonDataModel>(this.preUrlEndpoint + country);
