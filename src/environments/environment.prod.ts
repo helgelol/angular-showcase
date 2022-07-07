08f66f4ef01a92554e06518459007b0403c62938
helgelol <helge@kubes.no>
2022-07-07 16:15:56 +0200

    Firebase config 4 prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCRQ8qerKaQ7ku8Poo_pH01Mi8lNYjfiAM',
+    authDomain: 'angular-showcase-f0b16.firebaseapp.com',
+    projectId: 'angular-showcase-f0b16',
+    storageBucket: 'angular-showcase-f0b16.appspot.com',
+    messagingSenderId: '54118995304',
+    appId: '1:54118995304:web:b3aad221cfd3e73e4d32dd',
+  },
 };
